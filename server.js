f787efeb4bc38096f9c61c89503b9706a5b90e4f
katkinso <katyratkinson@yahoo.com>
2019-12-01 15:28:50 -0800

    cookie

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
+
+app.set('trust proxy', 1); // add this line
 app.get('/*', function (req, res) {
    res.sendFile(path.join(__dirname, 'build', 'index.html'));
  });
