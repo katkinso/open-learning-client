96e6c1c1d1727d6ad0e4051e38e9b7adcdd2e963
katkinso <katyratkinson@yahoo.com>
2019-12-01 08:24:30 -0800

    Fix header for no upcoming sessions

--- src/components/partials/SubHeader.js
+++ src/components/partials/SubHeader.js
@@ -5,20 +5,38 @@ import { Link } from 'react-router-dom'
 const Header = props => {
 
     const { nextSession } = props;
+    const nextSessionDateTime = nextSession ? utils.utcToLocal(nextSession.dateTime) : ""
+
+    const headline = () => {
+
+        if (nextSession){
+            return (
+            <div>
+                <h1>YOUR NEXT SESSION</h1>
+                <h2><Link className="text-brighter-blue" to={`/session/${nextSession.id}`}>
+                {nextSession.name}</Link></h2>
+                <h3 className="text-sm-muted">Total Spots: {nextSession.totalSpots}</h3>
+            </div>
+            )
+        }
+
+        return (<h2 className="text-brighter-blue">No upcoming sessions</h2>)
+    }
+
+    
 
     return (
         <header className="py-3 header-sub border-bottom">
             <div className="container">
                 <div className="row flex-nowrap justify-content-between align-items-center">
                     <div className="col-4 pt-1">
-                        <h1>YOUR NEXT SESSION</h1>
-                        <h2><Link className="text-brighter-blue" to={`/session/${nextSession.id}`}>
-                        {nextSession.name}</Link></h2>
-                        <h3 className="text-sm-muted">Total Spots: {nextSession.totalSpots}</h3>
+                        
+                        {headline()}
+                        
                     </div>
                     <div className="col-4 d-flex justify-content-end align-items-center">
                         <span className="font-weight-bold">
-                        {utils.utcToLocal(nextSession.dateTime)}
+                        {nextSessionDateTime}
                         </span>
                     </div>
                 </div>
