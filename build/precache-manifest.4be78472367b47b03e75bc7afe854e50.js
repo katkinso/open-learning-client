977b02bf60b17e913707ed9842312cd1a3287530
katkinso <katyratkinson@yahoo.com>
2019-12-01 09:33:57 -0800

    build folder

---
+++ build/precache-manifest.4be78472367b47b03e75bc7afe854e50.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d161fd0c4725f7823339f343faa95b7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfcd6510a726f661ce73",
+    "url": "/static/js/2.332eb7c3.chunk.js"
+  },
+  {
+    "revision": "56850f14f5f830ed2a34",
+    "url": "/static/js/main.dca495ba.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
